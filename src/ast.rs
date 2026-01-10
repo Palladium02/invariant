8beaae48cdb8091a5b8d4621b93d8f1a0811370a
Sean Meyer <seanmeyer544@gmail.com>
2026-01-10 16:31:45 +0100

    refactored ast types by converting tuple variants to field variant while also adding spans

--- src/ast.rs
+++ src/ast.rs
@@ -1,23 +1,72 @@
+use std::range::Range;
+
 pub enum Item {
-    Function(String, Vec<String>, Statement),
+    Function {
+        name: String,
+        arguments: Vec<String>,
+        body: Statement,
+        span: Range<usize>,
+    },
 }
 
 pub enum Statement {
-    Block(Vec<Statement>),
-    Binding(String, Expression),
-    Expression(Expression),
-    Return(Expression),
-    Branch(Expression, Box<Statement>, Option<Box<Statement>>),
-    While(Expression, Box<Statement>),
+    Block {
+        body: Vec<Statement>,
+        span: Range<usize>,
+    },
+    Binding {
+        bind_to: String,
+        value: Expression,
+        span: Range<usize>,
+    },
+    Expression {
+        expression: Expression,
+        span: Range<usize>,
+    },
+    Return {
+        value: Expression,
+        span: Range<usize>,
+    },
+    Branch {
+        condition: Expression,
+        then: Box<Statement>,
+        otherwise: Option<Box<Statement>>,
+        span: Range<usize>,
+    },
+    While {
+        condition: Expression,
+        body: Box<Statement>,
+        span: Range<usize>,
+    },
 }
 
 pub enum Expression {
-    Integer(i64),
-    Boolean(bool),
-    Reference(String),
-    Operation(Operation),
-    Assignment(String, Box<Expression>),
-    Call(String, Vec<Expression>),
+    Integer {
+        value: i64,
+        span: Range<usize>,
+    },
+    Boolean {
+        value: bool,
+        span: Range<usize>,
+    },
+    Reference {
+        name: String,
+        span: Range<usize>,
+    },
+    Operation {
+        operation: Operation,
+        span: Range<usize>,
+    },
+    Assignment {
+        assign_to: String,
+        value: Box<Expression>,
+        span: Range<usize>,
+    },
+    Call {
+        name: String,
+        arguments: Vec<Expression>,
+        span: Range<usize>,
+    },
 }
 
 pub enum Operation {
@@ -26,18 +75,54 @@ pub enum Operation {
 }
 
 pub enum BinaryOperation {
-    Add(Box<Expression>, Box<Expression>),
-    Sub(Box<Expression>, Box<Expression>),
-    Mul(Box<Expression>, Box<Expression>),
-    Div(Box<Expression>, Box<Expression>),
-    Equal(Box<Expression>, Box<Expression>),
-    NotEqual(Box<Expression>, Box<Expression>),
-    Less(Box<Expression>, Box<Expression>),
-    LessEqual(Box<Expression>, Box<Expression>),
-    Greater(Box<Expression>, Box<Expression>),
-    GreaterEqual(Box<Expression>, Box<Expression>),
-    And(Box<Expression>, Box<Expression>),
-    Or(Box<Expression>, Box<Expression>),
+    Add {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    Sub {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    Mul {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    Div {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    Equal {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    NotEqual {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    Less {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    LessEqual {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    Greater {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    GreaterEqual {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    And {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
+    Or {
+        lhs: Box<Expression>,
+        rhs: Box<Expression>,
+    },
 }
 
 pub enum UnaryOperation {
