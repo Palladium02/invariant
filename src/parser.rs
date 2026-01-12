f415257076b630eede40188113dce22d16aa3d1a
Sean Meyer <seanmeyer544@gmail.com>
2026-01-12 09:54:26 +0100

    finished baseline logic for top level function parsing, layed grounds for statement parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use crate::ast::{Item, Program};
+use crate::ast::{Item, Program, Statement};
 use crate::lexer::Lexer;
 use crate::token::{Token, TokenKind};
 use std::iter::Peekable;
@@ -32,7 +32,7 @@ impl<'t> Parser<'t> {
         Ok(Program::new(items))
     }
 
-    fn expect_item(&mut self) -> Result<Item, ()> {
+    fn expect_item(&mut self) -> Result<Item, ParseError> {
         match self.input.peek() {
             Some((Token::Fn, _)) => self.expect_function(),
             Some(_) => todo!(),
@@ -40,12 +40,13 @@ impl<'t> Parser<'t> {
         }
     }
 
-    fn expect_function(&mut self) -> Result<Item, ()> {
+    fn expect_function(&mut self) -> Result<Item, ParseError> {
         let _ = self.expect_token(TokenKind::Fn);
-        let name = self.expect_token(TokenKind::Identifier);
+        let name = self.expect_identifier()?;
         let _ = self.expect_token(TokenKind::LParen);
         let arguments = self.expect_arguments();
         let _ = self.expect_token(TokenKind::RParen);
+        let body = self.expect_block()?;
         todo!()
     }
 
@@ -56,16 +57,81 @@ impl<'t> Parser<'t> {
             return Ok(arguments);
         }
 
-        let (arg, _) = self.expect_token(TokenKind::Identifier)?;
-        arguments.push(arg.as_identifier_unchecked());
+        let (arg, _) = self.expect_identifier()?;
+        arguments.push(arg);
 
         while let Some((Token::Comma, _)) = self.input.peek() {
             self.input.next();
-            let (arg, _) = self.expect_token(TokenKind::Identifier)?;
-            arguments.push(arg.as_identifier_unchecked())
+            let (arg, _) = self.expect_identifier()?;
+            arguments.push(arg);
         }
 
         Ok(arguments)
+    }
+
+    fn expect_block(&mut self) -> Result<Statement, ParseError> {
+        let (_, start) = self.expect_token(TokenKind::LBrace)?;
+
+        if let Some((Token::RBrace, end)) = self.input.peek() {
+            return Ok(Statement::Block {
+                body: Vec::new(),
+                span: Range::from(start.start..end.end),
+            });
+        }
+
+        let mut statements = Vec::new();
+
+        while let Some((token, _)) = self.input.peek()
+            && token.kind() != TokenKind::RBrace
+        {
+            let statement = self.expect_statement()?;
+            statements.push(statement);
+        }
+
+        // eat unconsumed Token::RBrace
+        let end = match self.input.next() {
+            Some((Token::RBrace, span)) => span,
+            _ => return Err(ParseError::UndefinedFailure),
+        };
+
+        Ok(Statement::Block {
+            body: statements,
+            span: Range::from(start.start..end.end),
+        })
+    }
+
+    fn expect_statement(&mut self) -> Result<Statement, ParseError> {
+        match self.input.peek() {
+            Some((Token::LBrace, _)) => self.expect_block(),
+            Some((Token::Let, _)) => self.expect_binding(),
+            Some((Token::Return, _)) => self.expect_return(),
+            Some((Token::If, _)) => self.expect_branching(),
+            Some((Token::While, _)) => self.expect_while(),
+            _ => Err(ParseError::UndefinedFailure),
+        }
+    }
+
+    fn expect_binding(&mut self) -> Result<Statement, ParseError> {
+        todo!()
+    }
+
+    fn expect_return(&mut self) -> Result<Statement, ParseError> {
+        todo!()
+    }
+
+    fn expect_branching(&mut self) -> Result<Statement, ParseError> {
+        todo!()
+    }
+
+    fn expect_while(&mut self) -> Result<Statement, ParseError> {
+        todo!()
+    }
+
+    fn expect_identifier(&mut self) -> Result<(String, Range<usize>), ParseError> {
+        match self.input.next() {
+            Some((Token::Identifier(identifier), span)) => Ok((identifier, span)),
+            _ => Err(ParseError::UndefinedFailure),
+        }
     }
 
     fn expect_token(&mut self, kind: TokenKind) -> Result<(Token, Range<usize>), ParseError> {
